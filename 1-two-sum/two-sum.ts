d920d2c2ad462b89d21c1652004cd2ad9b583458
aga1611 <45217181+aga1611@users.noreply.github.com>
2025-06-04 18:39:58 +0100

    Time: 2 ms (74.97%) | Memory: 57.7 MB (25.07%) - LeetSync

--- 1-two-sum/two-sum.ts
+++ 1-two-sum/two-sum.ts
@@ -1,6 +1,6 @@
 // https://leetcode.com/problems/two-sum/description/
 
-function twoSum(nums: number[], target: number): number[] {
+function twoSumSlow(nums: number[], target: number): number[] {
     let arrayOfIndexes = []
     for(let i = 0; i < nums.length;  i++) {
          for(let j = 1; j <= nums.length;  j++) {
@@ -11,4 +11,21 @@ function twoSum(nums: number[], target: number): number[] {
     }
     }
     return arrayOfIndexes
+};
+
+function twoSum(nums: number[], target: number): number[] {
+    const hashMap = new Map();
+
+    for(let i = 0; i < nums.length;  i++) {
+        const complement = target - nums[i];
+
+        if (hashMap.has(complement) ) {
+            return [hashMap.get(complement), i]
+        }
+        hashMap.set(nums[i], i)
+    }
+    throw new Error(
+        "No solution"
+    )
+    
 };
