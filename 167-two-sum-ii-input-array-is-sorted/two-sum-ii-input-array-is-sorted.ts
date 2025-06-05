d720d00431a0a29a1febc9a3bfe8783c185a87ca
aga1611 <45217181+aga1611@users.noreply.github.com>
2025-06-05 20:46:49 +0100

    Time: 0 ms (100.00%) | Memory: 57 MB (41.66%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.ts
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.ts
@@ -1,4 +1,26 @@
 function twoSum(numbers: number[], target: number): number[] {
+    // IMPORTANT THAT IT"S already sorted
+    // set pointers one from beginning one from end
+    let left = 0;
+    let right = numbers.length - 1;
+
+    // run code until the 2 pointers meet
+    while(left < right) {
+        let candidate = numbers[left] + numbers[right];
+        if (candidate === target) {
+            return [left + 1, right + 1]
+        // if sum is higher than target, need lower numbers so moving right pointer to the left
+        } else if (candidate > target) {
+            right--;
+        } else {
+            left++;
+        }
+    }
+};
+
+
+
+function twoSumGoingFromTheFront(numbers: number[], target: number): number[] {
     // set pointers
     let left = 0;
     let right = 1;
