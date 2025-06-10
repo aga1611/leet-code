a98107af8ac7ede78ea9a1b382d39e8c91ea739f
aga1611 <45217181+aga1611@users.noreply.github.com>
2025-06-10 20:54:01 +0100

    Time: 3 ms (20.31%) | Memory: 58.9 MB (70.14%) - LeetSync

--- 3194-find-words-containing-character/find-words-containing-character.ts
+++ 3194-find-words-containing-character/find-words-containing-character.ts
@@ -1,4 +1,11 @@
 function findWordsContaining(words: string[], x: string): number[] {
+    return words
+        .map((word, index) => word.includes(x) ? index : -1)
+        .filter(index => index !== -1);
+}
+
+
+function findWordsContainingSlow(words: string[], x: string): number[] {
     const indexArray = []
     for(let i = 0; i < words.length; i++) {
         const splitWord = new Set(words[i].split(""));
