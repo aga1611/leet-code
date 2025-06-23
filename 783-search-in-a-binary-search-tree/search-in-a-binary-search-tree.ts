b2cda4bb7dac464b8b7cfaeac2175dbf6c55b5c1
aga1611 <45217181+aga1611@users.noreply.github.com>
2025-06-23 19:32:14 +0100

    Time: 0 ms (100.00%) | Memory: 62.8 MB (61.90%) - LeetSync

--- 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.ts
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.ts
@@ -12,14 +12,25 @@
  * }
  */
 
+// h -> height of tree
+// 	Time Complexity	O(h) 
+//  Space Complexity O(h)
 function searchBST(root: TreeNode | null, val: number): TreeNode | null {
+    // don't need to check root.left !== null  && root.right !== null as this condition already covers those cases
     if(!root) return null;
     if(root.val === val) return root;
-    if(val < root.val && root.left !== null) {
-        return searchBST(root.left, val)
-    } 
-    if(val > root.val && root.right !== null ) {
-        return searchBST(root.right, val)
-    } 
+    if(val < root.val) return searchBST(root.left, val)
+    if(val > root.val) return searchBST(root.right, val)
     return null
 };
+
+
+// 	Time Complexity	O(h) 
+//  Space Complexity O(1)  -> better as we don't have recursive call stack overhead
+function searchBSTIterative(root: TreeNode | null, val: number): TreeNode | null {
+    while (root !== null) {
+        if (root.val === val) return root;
+        root = val < root.val ? root.left : root.right;
+    }
+    return null;
+}
