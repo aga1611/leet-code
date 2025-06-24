fbef336ce0233eaf793064628e27041597a80e67
aga1611 <45217181+aga1611@users.noreply.github.com>
2025-06-24 22:36:51 +0100

    Time: 0 ms (100.00%) | Memory: 56.6 MB (37.15%) - LeetSync

--- 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.ts
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.ts
@@ -1,19 +1,27 @@
 function kidsWithCandies(candies: number[], extraCandies: number): boolean[] {
-    const result = [];
+    const maxCandies = Math.max(...candies);
+    return candies.map(c => c + extraCandies >= maxCandies);
+}
 
-    let maxCandies = 0;
 
-    for(let c of candies) {
-        maxCandies = Math.max(maxCandies, c)
-    }
 
-    for(let c of candies) {
-        if(maxCandies <= c + extraCandies) {
-            result.push(true)
-        } else {
-            result.push(false)
-        }
-    }
+// O(n)
+// function kidsWithCandies(candies: number[], extraCandies: number): boolean[] {
+//     const result = [];
 
-    return result;
-};
+//     let maxCandies = 0;
+
+//     for(let c of candies) {
+//         maxCandies = Math.max(maxCandies, c)
+//     }
+
+//     for(let c of candies) {
+//         if(maxCandies <= c + extraCandies) {
+//             result.push(true)
+//         } else {
+//             result.push(false)
+//         }
+//     }
+
+//     return result;
+// };
