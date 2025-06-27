c79c7b495aafa3266306244fc11947aa8ad97f7b
aga1611 <45217181+aga1611@users.noreply.github.com>
2025-06-27 20:43:47 +0100

    Time: 3 ms (37.27%) | Memory: 56.4 MB (88.94%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.ts
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.ts
@@ -1,3 +1,22 @@
 function reverseWords(s: string): string {
-    return s.split(" ").filter((w) => /\S/.test(w)).reverse().join(" ")
+    // split into array with separate words
+    let array = s.split(" ");
+    // create a new string
+    let reversedString = "";
+
+    // go through the array from the end
+    for(let i = array.length - 1; i >= 0; i--) {
+        // if it's space continue
+        if(array[i] === "") continue;
+        // if there is already a word in the string, add space
+        if(reversedString.length > 0) reversedString += " ";
+        // add word to the string
+        reversedString += array[i]
+    }
+    
+    return reversedString
 };
+
+// function reverseWords(s: string): string {
+//     return s.split(" ").filter((w) => /\S/.test(w)).reverse().join(" ")
+// };
