d517264791a0cef8e8fdc6296e1d1ef2355bde9d
aga1611 <45217181+aga1611@users.noreply.github.com>
2025-06-27 20:00:06 +0100

    Time: 0 ms (100.00%) | Memory: 57 MB (71.57%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.ts
+++ 42-trapping-rain-water/trapping-rain-water.ts
@@ -1,19 +1,35 @@
+// Key Insight: Water trapped at a position depends on the shorter of the tallest bars to its left and right
+
+
+// O(n) time and O(1) space complexity
 function trap(height: number[]): number {
+    // 2 pointer, one at the begining, one at the end
     let left = 0;
     let right = height.length - 1;
 
+    // tracks the highest bar seen from the left up to the current position.
     let leftMax = 0
+    // tracks the highest bar seen from the right up to the current position.
     let rightMax = 0;
+    // water total
     let trappedWater = 0;
 
+    // A loop that continues until the two pointers meet in the middle.
     while(left < right) {
+        // Updates leftMax and rightMax with the maximum values seen so far from their respective directions.
         leftMax = Math.max(leftMax, height[left]);
         rightMax = Math.max(rightMax, height[right]);
 
+        // Decides which pointer to move
+        //  The key insight:
+        //  Water trapped at a point depends on the shorter side.
+        //  So, move the side with the smaller current height inward.
         if(height[left] < height[right]) {
+            // calculate water trapped at left
             trappedWater += leftMax - height[left]
             left++;
         } else {
+            // calculate water trapped at right
             trappedWater += rightMax - height[right]
             right--;
         }
