8411489aab215a95410159d13079592693d0c209
aga1611 <45217181+aga1611@users.noreply.github.com>
2025-06-27 19:54:47 +0100

    Time: 1 ms (69.03%) | Memory: 57.8 MB (44.14%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.ts
+++ 42-trapping-rain-water/trapping-rain-water.ts
@@ -1,32 +1,58 @@
 function trap(height: number[]): number {
-    // calcualte max value when going from left to right
-    let forwardMax = [];
-    let max = 0;
+    let left = 0;
+    let right = height.length - 1;
 
-    for (let h of height) {
-        max = Math.max(h, max);
-        forwardMax.push(max)
-    }
+    let leftMax = 0
+    let rightMax = 0;
+    let trappedWater = 0;
 
-    // // calcualte max value when going from right to left
-    let backwardsMax = [];
-    let max2 = 0;
+    while(left < right) {
+        leftMax = Math.max(leftMax, height[left]);
+        rightMax = Math.max(rightMax, height[right]);
 
-    for (let i = height.length - 1; i >= 0; i--) {
-        max2 = Math.max(height[i], max2);
-        backwardsMax.unshift(max2)
-    }
-
-    let water: number = 0;
-    for (let i = 0; i < height.length; i++) {
-        // check what's the min height at that block
-        let heightChange = Math.min(forwardMax[i], backwardsMax[i])
-        // calculate if there could be any water there
-        let waterInPlace = heightChange - height[i];
-        // add to toatal if there is some extra water
-        if (waterInPlace > 0) {
-            water = water + waterInPlace
+        if(height[left] < height[right]) {
+            trappedWater += leftMax - height[left]
+            left++;
+        } else {
+            trappedWater += rightMax - height[right]
+            right--;
         }
     }
-    return water
-};
+
+    return trappedWater
+}
+
+
+// // works but not efficient
+// function trap(height: number[]): number {
+//     // calcualte max value when going from left to right
+//     let forwardMax = [];
+//     let max = 0;
+
+//     for (let h of height) {
+//         max = Math.max(h, max);
+//         forwardMax.push(max)
+//     }
+
+//     // // calcualte max value when going from right to left
+//     let backwardsMax = [];
+//     let max2 = 0;
+
+//     for (let i = height.length - 1; i >= 0; i--) {
+//         max2 = Math.max(height[i], max2);
+//         backwardsMax.unshift(max2)
+//     }
+
+//     let water: number = 0;
+//     for (let i = 0; i < height.length; i++) {
+//         // check what's the min height at that block
+//         let heightChange = Math.min(forwardMax[i], backwardsMax[i])
+//         // calculate if there could be any water there
+//         let waterInPlace = heightChange - height[i];
+//         // add to toatal if there is some extra water
+//         if (waterInPlace > 0) {
+//             water = water + waterInPlace
+//         }
+//     }
+//     return water
+// };
