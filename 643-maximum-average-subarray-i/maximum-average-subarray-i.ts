113c0928ec9a32aff7c06dc760b612afbde83e7e
aga1611 <45217181+aga1611@users.noreply.github.com>
2025-06-29 13:06:28 +0100

    Time: 3 ms (48.37%) | Memory: 69.6 MB (52.63%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.ts
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.ts
@@ -1,5 +1,29 @@
 function findMaxAverage(nums, k) {
     let sum = 0;
+
+    for (let i = 0; i < k; i++) {
+        sum += nums[i]
+    }
+
+    let maxSum = sum;
+
+    let left = 0;
+    let right = k;
+
+    while (right < nums.length) {
+        sum =  sum - nums[left] + nums[right];
+        maxSum = Math.max(sum, maxSum);
+
+        left++
+        right++
+    }
+
+    return maxSum / k //(avg)
+}
+
+/*
+function findMaxAverage(nums, k) {
+    let sum = 0;
     // calculating sum for the first window
     for(let i = 0; i < k; i++) {
         sum +=nums[i]
@@ -22,3 +46,4 @@ function findMaxAverage(nums, k) {
     // returning the averge of max Sum
     return maxSum / k;
 };
+*/
