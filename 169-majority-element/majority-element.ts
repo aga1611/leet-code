79fc2de7f43e10ce584b981ff8fbfbde2deab638
aga1611 <45217181+aga1611@users.noreply.github.com>
2025-06-29 21:48:57 +0100

    Time: 0 ms (100.00%) | Memory: 60.1 MB (20.79%) - LeetSync

---
+++ 169-majority-element/majority-element.ts
@@ -0,0 +1,15 @@
+// Boyer–Moore majority vote algorithm
+// works if you have guaranteed majority element
+function majorityElement(nums: number[]): number {
+    let count = 0;
+    let candidate: number | null = null;
+
+    for (const num of nums) {
+        if (count === 0) {
+            candidate = num;
+        }
+        count += (num === candidate) ? 1 : -1;
+    }
+
+    return candidate!;
+}
