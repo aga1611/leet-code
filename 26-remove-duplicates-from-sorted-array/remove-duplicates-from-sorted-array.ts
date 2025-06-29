20f5d0f12b8db29c0476d1049819876065fabdea
aga1611 <45217181+aga1611@users.noreply.github.com>
2025-06-29 21:23:55 +0100

    Time: 1 ms (59.43%) | Memory: 57.1 MB (96.45%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.ts
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.ts
@@ -1,3 +1,22 @@
+// this is enough if the array is sorted
+function removeDuplicates(nums: number[]): number {
+    if (nums.length === 0) return 0;
+    let k = 1; // index for next unique element
+
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] !== nums[i - 1]) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+
+    return k;
+}
+
+
+/*
+// this works for non sorted array
+// worse space complexity as we need to create a set
 function removeDuplicates(nums: number[]): number {
     let k = 0;
     let visited = new Set();
@@ -13,3 +32,4 @@ function removeDuplicates(nums: number[]): number {
     return k
     
 };
+*/
