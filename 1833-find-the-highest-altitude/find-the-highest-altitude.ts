58577c5b585030bd209f08aaa4e7f612a2be58b1
aga1611 <45217181+aga1611@users.noreply.github.com>
2025-07-05 17:24:11 +0100

    Time: 1 ms (28.88%) | Memory: 55.6 MB (30.55%) - LeetSync

--- 1833-find-the-highest-altitude/find-the-highest-altitude.ts
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.ts
@@ -1,4 +1,21 @@
+// Time O(N) Space O(1)
 function largestAltitude(gain: number[]): number {
+    let maxAltitude = 0;
+    let currentAltitude = 0;
+
+    for (let g of gain) {
+        currentAltitude += g;
+        maxAltitude = Math.max(maxAltitude, currentAltitude);
+    }
+
+    return maxAltitude;
+}
+
+
+/*
+// works but space & time complexity is O(N) as we create new prefixSum array
+function largestAltitude(gain: number[]): number {
+
     const prefixSum = new Array(gain.length + 1).fill(0);
     prefixSum[0] = 0;
 
@@ -10,3 +27,4 @@ function largestAltitude(gain: number[]): number {
 
     return maxAttitude
 };
+*/
